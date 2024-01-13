42d898ded5b8def6fcccf13d8890598a85dbf092
Daniel Cavalcante <dvictor.cintra@gmail.com>
2024-01-12 23:30:17 -0300

    add textStyles and optional icon to CustomButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,7 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image"
 
-const CustomButton = ({ title, containerStyles, handleClick, buttonType }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, buttonType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
@@ -11,10 +11,20 @@ const CustomButton = ({ title, containerStyles, handleClick, buttonType }: Custo
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
 
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image
+              src={rightIcon}
+              alt="arrow_left"
+              fill
+              className="object-contain"
+            />
+          </div>
+        )}
     </button>
   )
 }
